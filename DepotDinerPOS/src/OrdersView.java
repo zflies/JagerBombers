7c9831b454ef6502f7c1c238f3918d8672536e14
zflies <zachflies11@gmail.com>
2014-02-24 09:50:57 -0600

    Added JTabbedPane to Orders View

--- DepotDinerPOS/src/OrdersView.java
+++ DepotDinerPOS/src/OrdersView.java
@@ -19,15 +19,19 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JTabbedPane;
 
 
 public class OrdersView extends JFrame {
 
 	private static OrdersView frame;
-	private JPanel contentPane, panelOrders;
-	private JButton btnExit, btnCreateOrder, btnHistory;
+	private JPanel contentPane;
+	private JButton btnExit;
 	private JLabel lblEmployee;
-	private JList listOrders;
+	private JPanel panelOrders;
+	private JButton button;
+	private JButton button_1;
+	private JList list;
 
 
 	/**
@@ -57,8 +61,6 @@ public class OrdersView extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		panelOrders = new JPanel();
-		
 		btnExit = new JButton("EXIT");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -74,62 +76,69 @@ public class OrdersView extends JFrame {
 		lblEmployee = new JLabel("EMPLOYEE:  ZACH FLIES");
 		lblEmployee.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		lblEmployee.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(panelOrders, GroupLayout.DEFAULT_SIZE, 862, Short.MAX_VALUE))
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
 							.addGap(442)
-							.addComponent(lblEmployee, GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE)
+							.addComponent(lblEmployee, GroupLayout.DEFAULT_SIZE, 330, Short.MAX_VALUE)
 							.addGap(18)
-							.addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)))
+							.addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, 862, Short.MAX_VALUE)))
 					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
 						.addComponent(lblEmployee, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(panelOrders, GroupLayout.DEFAULT_SIZE, 580, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(tabbedPane, GroupLayout.DEFAULT_SIZE, 586, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		
-		listOrders = new JList();
+		panelOrders = new JPanel();
+		tabbedPane.addTab("ORDERS", null, panelOrders, null);
 		
-		btnCreateOrder = new JButton("CREATE ORDER");
-		btnCreateOrder.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		button = new JButton("HISTORY");
+		button.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		
-		btnHistory = new JButton("HISTORY");
-		btnHistory.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		button_1 = new JButton("CREATE ORDER");
+		button_1.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		
+		list = new JList();
 		GroupLayout gl_panelOrders = new GroupLayout(panelOrders);
 		gl_panelOrders.setHorizontalGroup(
-			gl_panelOrders.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panelOrders.createSequentialGroup()
+			gl_panelOrders.createParallelGroup(Alignment.TRAILING)
+				.addGap(0, 862, Short.MAX_VALUE)
+				.addGroup(gl_panelOrders.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panelOrders.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_panelOrders.createSequentialGroup()
-							.addComponent(btnHistory, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
+							.addComponent(button, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED, 526, Short.MAX_VALUE)
-							.addComponent(btnCreateOrder))
-						.addComponent(listOrders, GroupLayout.DEFAULT_SIZE, 850, Short.MAX_VALUE))
+							.addComponent(button_1))
+						.addComponent(list, GroupLayout.DEFAULT_SIZE, 850, Short.MAX_VALUE))
 					.addContainerGap())
 		);
 		gl_panelOrders.setVerticalGroup(
 			gl_panelOrders.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 580, Short.MAX_VALUE)
 				.addGroup(gl_panelOrders.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(listOrders, GroupLayout.DEFAULT_SIZE, 494, Short.MAX_VALUE)
+					.addComponent(list, GroupLayout.DEFAULT_SIZE, 494, Short.MAX_VALUE)
 					.addGap(28)
 					.addGroup(gl_panelOrders.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnCreateOrder, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnHistory, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE))
+						.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+						.addComponent(button, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap())
 		);
 		panelOrders.setLayout(gl_panelOrders);
@@ -137,5 +146,4 @@ public class OrdersView extends JFrame {
 		
 		
 	}
-
 }
