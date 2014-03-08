a10ca45c73a5d53d34639683d01b28e9db44a76e
Jeffrey Rondeau <j.rondeau16@gmail.com>
2014-03-08 16:31:29 -0600

    Add custom event handler for selection within a table Make employee list pull from database Make selected employee's name dynamic with label

--- DepotDinerPOS/src/ManageView.java
+++ DepotDinerPOS/src/ManageView.java
@@ -1,24 +1,31 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JDialog;
 import javax.swing.JTabbedPane;
-import javax.swing.JDesktopPane;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ListSelectionModel;
 
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
-import javax.swing.JList;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
 
 
 public class ManageView extends JFrame {
@@ -28,6 +35,13 @@ public class ManageView extends JFrame {
 	private static ManageView frame;
 	
 	private String employeeName;
+	private JTable employeeTable;
+	private JLabel lblEmployeeName;
+	private String selectedEmployeeFirstName;
+	private String selectedEmployeeLastName;
+	private SharedListSelectionHandler selectionHandler;
+	ListSelectionModel listSelectionModel;
+	String column_names[]= {"First Name","Last Name"};
 
 	/**
 	 * Create the frame.
@@ -86,8 +100,6 @@ public class ManageView extends JFrame {
 		JPanel panel_manage = new JPanel();
 		tabbedPane.addTab("Manage", null, panel_manage, null);
 		
-		JList list = new JList();
-		
 		JButton btnAddEmployee = new JButton("Add Employee");
 		btnAddEmployee.addMouseListener(new MouseAdapter() {
 			@Override
@@ -102,8 +114,9 @@ public class ManageView extends JFrame {
 			}
 		});
 		
-		JLabel lblNewLabel = new JLabel("Jeffrey Allen Rondeau");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		lblEmployeeName = new JLabel("");
+		lblEmployeeName.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEmployeeName.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		
 		JButton btnClockIn = new JButton("Clock In");
 		
@@ -116,35 +129,31 @@ public class ManageView extends JFrame {
 		JButton btnDelete = new JButton("Delete");
 		
 		JButton btnGenerateReport = new JButton("Generate Report");
+		
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout gl_panel_manage = new GroupLayout(panel_manage);
 		gl_panel_manage.setHorizontalGroup(
-			gl_panel_manage.createParallelGroup(Alignment.LEADING)
+			gl_panel_manage.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel_manage.createSequentialGroup()
-					.addGap(21)
-					.addComponent(list, GroupLayout.PREFERRED_SIZE, 272, GroupLayout.PREFERRED_SIZE)
-					.addGroup(gl_panel_manage.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_manage.createParallelGroup(Alignment.LEADING)
-							.addGroup(gl_panel_manage.createParallelGroup(Alignment.LEADING)
+					.addGap(20)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 311, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 145, Short.MAX_VALUE)
+					.addGroup(gl_panel_manage.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_panel_manage.createSequentialGroup()
+							.addGroup(gl_panel_manage.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(btnViewOrders, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 								.addGroup(gl_panel_manage.createSequentialGroup()
-									.addPreferredGap(ComponentPlacement.RELATED, 226, Short.MAX_VALUE)
-									.addComponent(lblNewLabel)
-									.addGap(126))
-								.addGap(237))
-							.addGroup(Alignment.TRAILING, gl_panel_manage.createSequentialGroup()
-								.addGap(183)
-								.addGroup(gl_panel_manage.createParallelGroup(Alignment.LEADING, false)
-									.addComponent(btnViewOrders, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addGroup(gl_panel_manage.createSequentialGroup()
-										.addComponent(btnClockIn, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)
-										.addGap(77)
-										.addComponent(btnClockOut))
-									.addComponent(btnViewPayrollhours, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-								.addGap(75)
-								.addGap(10)))
-						.addGroup(Alignment.TRAILING, gl_panel_manage.createSequentialGroup()
-							.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(btnClockIn, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE)
+									.addGap(77)
+									.addComponent(btnClockOut))
+								.addComponent(btnViewPayrollhours, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+							.addGap(85))
+						.addGroup(gl_panel_manage.createSequentialGroup()
 							.addComponent(btnDelete)
-							.addGap(183))))
+							.addGap(183))
+						.addGroup(gl_panel_manage.createSequentialGroup()
+							.addComponent(lblEmployeeName)
+							.addGap(176))))
 				.addGroup(gl_panel_manage.createSequentialGroup()
 					.addGap(89)
 					.addComponent(btnAddEmployee)
@@ -156,11 +165,10 @@ public class ManageView extends JFrame {
 			gl_panel_manage.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_manage.createSequentialGroup()
 					.addGap(22)
-					.addGroup(gl_panel_manage.createParallelGroup(Alignment.BASELINE)
-						.addComponent(list, GroupLayout.DEFAULT_SIZE, 289, Short.MAX_VALUE)
-						.addGroup(gl_panel_manage.createSequentialGroup()
-							.addComponent(lblNewLabel)
-							.addGap(22)
+					.addGroup(gl_panel_manage.createParallelGroup(Alignment.TRAILING)
+						.addGroup(Alignment.LEADING, gl_panel_manage.createSequentialGroup()
+							.addComponent(lblEmployeeName)
+							.addGap(32)
 							.addGroup(gl_panel_manage.createParallelGroup(Alignment.BASELINE)
 								.addComponent(btnClockIn)
 								.addComponent(btnClockOut))
@@ -169,18 +177,120 @@ public class ManageView extends JFrame {
 							.addGap(17)
 							.addComponent(btnViewPayrollhours)
 							.addGap(14)
-							.addComponent(btnDelete)))
-					.addGap(18)
+							.addComponent(btnDelete)
+							.addGap(79))
+						.addGroup(gl_panel_manage.createSequentialGroup()
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 308, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)))
 					.addGroup(gl_panel_manage.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnAddEmployee)
 						.addComponent(btnGenerateReport))
-					.addGap(12))
+					.addGap(11))
 		);
+		
+		DefaultTableModel table_model= new DefaultTableModel(column_names, 0);
+		employeeTable = new JTable(table_model);
+		employeeTable.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("ACTUALLY CLICKED ENTRY");
+				updateEmployeeName();
+			}
+		});
+		listSelectionModel = employeeTable.getSelectionModel();
+		selectionHandler = new SharedListSelectionHandler();
+        listSelectionModel.addListSelectionListener(selectionHandler);
+        employeeTable.setSelectionModel(listSelectionModel);
+		employeeTable.setFont(new Font("Lucida Grande", Font.BOLD, 15));
+		scrollPane.setViewportView(employeeTable);
 		panel_manage.setLayout(gl_panel_manage);
 		
 		JPanel panel_orders = new JPanel();
 		tabbedPane.addTab("Orders", null, panel_orders, null);
 		contentPane.setLayout(gl_contentPane);
 		//tabbedPane.setEnabledAt(1, false);
+		refreshEmployeeTable();
+		populateEmployeeName();
 	}
+	
+	private void refreshEmployeeTable(){
+		List<String> employeeList = sortEmployeeList(getEmployees());
+		DefaultTableModel model = (DefaultTableModel) employeeTable.getModel();
+		for(int i = 0; i < employeeList.size(); i++){
+			String[] employeeArray = employeeList.get(i).split(",");
+			model.addRow(new Object[]{employeeArray[0], employeeArray[1]});
+		}
+	}
+	
+	private void populateEmployeeName(){
+		List<String> employeeList = sortEmployeeList(getEmployees());
+		lblEmployeeName.setText(employeeList.get(0));
+	}
+	
+	private List<String> getEmployees(){
+		List<String> employeeList = new ArrayList<String> ();
+		String entry;
+		java.sql.Statement state = DBConnection.OpenConnection();
+		String commandstring = "SELECT FirstName, LastName FROM avalenti.Employees;";
+		if(state != null){
+			try {
+				ResultSet rs = state.executeQuery(commandstring);
+				while(rs.next() == true) {
+					String firstName = rs.getString("FirstName");
+					String lastName = rs.getString("LastName");
+					entry = lastName + "," + firstName;
+					employeeList.add(entry);
+				}
+			} catch (SQLException e) {
+				System.err.println("Error in SQL Execution");
+				}
+		}
+		else
+			System.err.println("Statement was null.  No connection?");
+		
+		return employeeList;
+	}
+	
+	private List<String> sortEmployeeList(List<String> employeeList){
+		java.util.Collections.sort(employeeList);
+		return employeeList;
+	}
+	
+	private void updateEmployeeName(){
+		 ListSelectionModel model = employeeTable.getSelectionModel();
+		 selectedEmployeeFirstName = (String) employeeTable.getValueAt(model.getLeadSelectionIndex(), 1);
+		 selectedEmployeeLastName = (String) employeeTable.getValueAt(model.getLeadSelectionIndex(), 0);
+		 lblEmployeeName.setText(selectedEmployeeLastName + ", " + selectedEmployeeFirstName);
+	}
+		
+	class SharedListSelectionHandler implements ListSelectionListener {
+        public void valueChanged(ListSelectionEvent e) {
+            ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+            int firstIndex = e.getFirstIndex();
+            int lastIndex = e.getLastIndex();
+            boolean isAdjusting = e.getValueIsAdjusting();
+            System.out.println("Event for indexes "
+                          + firstIndex + " - " + lastIndex
+                          + "; isAdjusting is " + isAdjusting
+                          + "; selected indexes:");
+ 
+            if (lsm.isSelectionEmpty()) {
+            	System.out.println(" <none>");
+            } else {
+                // Find out which indexes are selected.
+                int minIndex = lsm.getMinSelectionIndex();
+                int maxIndex = lsm.getMaxSelectionIndex();
+                for (int i = minIndex; i <= maxIndex; i++) {
+                    if (lsm.isSelectedIndex(i)) {
+                    	System.out.println(" " + i);
+                    	selectedIndex = i;
+                    }
+                }
+            }
+            System.out.println();
+        }
+        
+        public int selectedIndex;
+    }
+	
 }
